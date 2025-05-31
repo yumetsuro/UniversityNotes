9f09f064b53709deb072223f1ba1af41d1afe095
U-n-Own <cenzogargano@gmail.com>
2025-05-31 14:08:13 +0200

    lil fix

--- Assignments/ParallelDistributedSystems/Assignment4/src/modular_ff_mpi/main.cpp
+++ Assignments/ParallelDistributedSystems/Assignment4/src/modular_ff_mpi/main.cpp
@@ -321,8 +321,9 @@ int main(int argc, char* argv[]) {
     int step = 1;
     while (step < num_nodes) {
         // Print the step to understand the merging process
-        if (DEBUG)
+        if (DEBUG){
             printf("[RANK %d] MPI merge step: %d\n", rank, step);
+        }
         
             if (rank % (2 * step) == 0) {
             // Receiver process : 0,2,4,...,num_nodes-1
